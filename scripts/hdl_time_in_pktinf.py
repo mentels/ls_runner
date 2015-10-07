01cc8ee2ff6d424e7ecf39e635e56eee8468f01e
Szymon Mentel <szymon.mentel@erlang-solutions.com>
2015-10-07 11:41:19 +0200

    Add sorting the values

--- scripts/hdl_time_in_pktinf.py
+++ scripts/hdl_time_in_pktinf.py
@@ -50,19 +50,25 @@ def aggregate_metrics(child_dir):
       f.write(pline + '\n')
   [f.close() for f in files]
 
-def compute(child_dir, base_dir):
+def compute(child_dir, base_dir, acc):
   data = np.loadtxt(path(child_dir, COMBOUT))
   avgs = np.mean(data, axis=0)
   stds = np.std(data, axis=0)
+  acc.append(zip(avgs, stds))
+  return acc
+
+def store_data(base_dir, avgs_with_stds_list):
   with open(path(base_dir, data_filename(base_dir)), 'a') as out:
-    for value,std in zip(avgs, stds):
-      out.write("%.3f\t %.3f\t" % (value/1000, std/1000))
-    out.write("\n")
+    for avgs_with_stds in avgs_with_stds_list:
+      for i,(value,std) in enumerate(avgs_with_stds):
+        out.write("%.3f,%.3f" % (value/1000, std/1000))
+        if i<2:
+          out.write(",")
+      out.write("\n")
 
 def prepare_out(base_dir):
   with open(path(base_dir, data_filename(base_dir)), 'w') as out:
-    out.write("# pktin\t pktin_std\t app\t app_std\t ctrl\t ctrl_std"\
-              "# pkt in in thousands; times in milis\n")
+    out.write("pktin,pktin_std,app,app_std,ctrl,ctrl_std\n")
 
 def plot(base_dir):
   cmd = ('gnuplot -e "output_plot=\'{output_plot}\'"' +
@@ -77,11 +83,17 @@ def plot(base_dir):
   if result:
     logging.info("Plot cmd result %s" % result)
 
+def sort_by_pktin(avgs_with_stds_list):
+  avgs_with_stds_list.sort(key = lambda t: t[0][0])
+  return avgs_with_stds_list
+
 def main(base_dir):
   prepare_out(base_dir)
-  for d in get_child_dirs(base_dir, "_2015"):
+  acc = []
+  for d in get_child_dirs(base_dir, "*2015"):
     aggregate_metrics(d)
-    compute(d, base_dir)
+    acc = compute(d, base_dir, acc)
+  store_data(base_dir, sort_by_pktin(acc))
   plot(base_dir)
 
 if __name__ == '__main__':
